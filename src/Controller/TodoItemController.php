171da5ef9d5987ba7d481a95be10cd97b269c406
Damien <damien.lebre88@gmail.com>
2022-04-12 16:26:13 +0200

    suite

--- src/Controller/TodoItemController.php
+++ src/Controller/TodoItemController.php
@@ -4,9 +4,11 @@ namespace App\Controller;
 
 
 use App\Repository\TodoItemRepository;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class TodoItemController extends AbstractController
 {
@@ -26,5 +28,18 @@ class TodoItemController extends AbstractController
     {
             $Todo = $todoRepository->findOneBy(['id' => $id]);
         return $this->render('todo_item/show.html.twig', ["todo"=>$Todo]);
+    }
+
+      /**
+     * @Route("/{id}", name="todo_modify")
+     */
+    public function edit(TodoItemRepository $todoRepository, Request $request, EntityManagerInterface $em): Response
+    {
+        $form = $this->createForm(TodoType::class, $todo);
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()){
+          
+        }
     }
 }
